fdf19c26021ac9c60923497aa90ac5ce2097696d
martinmramos <xitej_@hotmail.com>
2022-06-04 13:24:37 +0200

    Prueba de Parque de Atracciones

---
+++ src/ProgramacionObjetos/ParqueAtracciones.java
@@ -0,0 +1,8 @@
+package ProgramacionObjetos;
+
+public class ParqueAtracciones {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
---
+++ src/ProgramacionObjetos/Ticket.java
@@ -0,0 +1,2 @@
+package ProgramacionObjetos;public class Ticket {
+}
